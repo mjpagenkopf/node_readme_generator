66b7753afa7182f07551e260e6cfe07a7347812e
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-04-17 20:02:00 -0400

    created the prompt array

--- index.js
+++ index.js
@@ -6,32 +6,76 @@ console.log(inquirer);
 console.log(fs);
 inquirer //
   .prompt([
-    {
+    { //displaying project title of README
       type: 'input',
-      message: 'What is your user name?',
-      name: 'nameAnswer',
+      message: 'What is your project title?',
+      name: 'title',
     },
-    {
+    { //section for description
       type: 'input',
-      message: 'What is your location?',
-      name: 'location',
+      message: 'Enter description of your project',
+      name: 'Description',
     },
-    {
+    { //section for installation instructions
       type: 'input',
-      message: 'Tell me about yourself',
-      name: 'bio',
+      message: 'Enter installation instructions',
+      name: 'Install',
     },
-    {
-        type: 'input',
-        message: 'please provide LinkedIn URL',
-        name: 'URL',
-      },
-      {
-        type: 'input',
-        message: 'please provide Github URL',
-        name: 'URL2',
-      },
+    { //section for usage information
+      type: 'input',
+      message: 'Enter usage information',
+      name: 'Usage',
+    },
+    { //section for contribution guidelines
+      type: 'input',
+      message: 'Enter contribution guidelines',
+      name: 'Contributing',
+    },
+    { //section for test instructions
+      type: 'input',
+      message: 'Enter test instructions',
+      name: 'Tests',
+    },
+    { //section for choosing License
+      type: 'checkbox',
+      message: 'Choose your license',
+      name: 'License',
+      choices: [
+        new inquirer.Separator(' Licenses '),
+        {
+          name: 'Apache License v2.0',
+        },
+        {
+          name: 'GNU General Public License v3.0',
+        },
+        {
+          name: 'MIT License',
+        },
+        ],
+          validate: function (answer) {
+          if (answer.length < 1) {
+              return 'You must choose at a license';
+            }
+            return true;
+        },
+    },
+    { //Questions section with link to Github
+      type: 'input',
+      message: 'Enter Github username', 
+      name: 'Username',
+    },
+    { //Questions section with instructions on how to reach 
+      type: 'input',
+      message: 'Enter email address', 
+      name: 'email',
+    },
+    { //table of Contents section that has links to corresonding section of README
+      type: 'list',
+      message: 'Table of content',
+      name: 'Tests',
+    },  
   ])
+  
   .then((response) => {
     //1st step is to create variable and with template literals, paste initial html
     var htmlString = `<!DOCTYPE html> 
