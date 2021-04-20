6615d3215b5ff05074a9dde920702e7e39d13f83
mjpagenkopf <78058404+mjpagenkopf@users.noreply.github.com>
2021-04-19 22:15:31 -0400

    index.js successfully creates readme file

---
+++ generatedReadMe/README.md
@@ -0,0 +1,48 @@
+
+    # 
+    lisjfi
+
+
+    ## Description
+
+    lsijgli
+
+
+    ## Install
+
+    laigliagj
+
+
+    ## Usage Instructions
+
+    laijegij
+
+
+    ## Contribution Guidelines
+
+    undefined
+
+
+    ## Test Instructions
+
+    undefined
+
+
+    ## License
+
+    undefined
+
+
+    ## Username
+
+    liajf
+
+
+    ## Email
+
+    laij
+
+
+    ## table of contents 
+
+    alif
--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const fs = require("fs");
-
+const path = require('path');
 const inquirer = require("inquirer");
 
-console.log(inquirer);
-console.log(fs);
-inquirer //
+const generator = (markdown) => {
+console.log(markdown);
+return    `
+    # 
+    ${markdown.title}\n
+
+    ## Description\n
+    ${markdown.description}\n
+
+    ## Install\n
+    ${markdown.install}\n
+
+    ## Usage Instructions\n
+    ${markdown.usage}\n
+
+    ## Contribution Guidelines\n
+    ${markdown.contributing}\n
+
+    ## Test Instructions\n
+    ${markdown.tests}\n
+
+    ## License\n
+    ${markdown.choices}\n
+
+    ## Username\n
+    ${markdown.Username}\n
+
+    ## Email\n
+    ${markdown.email}\n
+
+    ## table of contents \n
+    ${markdown.table}`;
+}
+
+inquirer
   .prompt([
     { //displaying project title of README
       type: 'input',
@@ -14,17 +46,17 @@ inquirer //
     { //section for description
       type: 'input',
       message: 'Enter description of your project',
-      name: 'Description',
+      name: 'description',
     },
     { //section for installation instructions
       type: 'input',
       message: 'Enter installation instructions',
-      name: 'Install',
+      name: 'install',
     },
     { //section for usage information
       type: 'input',
       message: 'Enter usage information',
-      name: 'Usage',
+      name: 'usage',
     },
     { //section for contribution guidelines
       type: 'input',
@@ -37,27 +69,30 @@ inquirer //
       name: 'Tests',
     },
     { //section for choosing License
-      type: 'checkbox',
+      type: 'list',
       message: 'Choose your license',
       name: 'License',
       choices: [
-        new inquirer.Separator(' Licenses '),
-        {
-          name: 'Apache License v2.0',
-        },
-        {
-          name: 'GNU General Public License v3.0',
-        },
-        {
-          name: 'MIT License',
-        },
+        //   new inquirer.Separator('License'),
+        // {
+        //   name: 
+          'Apache License v2.0',
+        // },
+        // {
+        //   name: 
+          'GNU General Public License v3.0',
+        // },
+        // {
+        //   name: 
+          'MIT License',
+        // },
         ],
-          validate: function (answer) {
-          if (answer.length < 1) {
-              return 'You must choose at a license';
-            }
-            return true;
-        },
+        //   validate: function (answer) {
+        //   if (answer.length < 1) {
+        //       return 'You must choose at a license';
+        //     }
+        //     return true;
+        // },
     },
     { //Questions section with link to Github
       type: 'input',
@@ -70,39 +105,78 @@ inquirer //
       name: 'email',
     },
     { //table of Contents section that has links to corresonding section of README
-      type: 'list',
+      type: 'input',
       message: 'Table of content',
-      name: 'Tests',
+      name: 'table',
     },  
   ])
-  
-  .then((response) => {
-    //1st step is to create variable and with template literals, paste initial html
-    var htmlString = `<!DOCTYPE html> 
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>User Portfolio</title>
-    </head>
-    <body>
-        <div class='container'>
-        <h1>${response.nameAnswer}</h1>
-        <h2>${response.location}</h2>
-        <h2>${response.bio}</h2>
+// };
+// generator()
+//   .then(response => {
+    //1st step is to create variable and with template literals
+    .then((markdown) => {
+        const readMeString = generator(markdown);
         
-        </div>
-    </body>
-    </html>`
+        fs.writeFile(path.resolve(__dirname, './generatedReadMe/README.md'), readMeString, (err) => 
+    err ? console.error(err) : console.log('Success!')
+  ); 
+});
+
+    // var choices = ''; 
+    // const readMeString = (`
+
+    // # 
+    // ${response.title}\n
+
+    // ## Description\n
+    // ${response.description}\n
+
+    // ## Install\n
+    // ${response.install}\n
+
+    // ## Usage Instructions\n
+    // ${response.usage}\n
+
+    // ## Contribution Guidelines\n
+    // ${response.contributing}\n
+
+    // ## Test Instructions\n
+    // ${response.tests}\n
+
+    // ## License\n
+    // ${response.choices}\n
+
+    // ## Username\n
+    // ${response.Username}\n
+
+    // ## Email\n
+    // ${response.email}\n
+
+    // ## table of contents \n
+    // ${response.table}`)
+
+//     fs.writeFile('README.md', readMeString, (err) => 
+//     err ? console.error(err) : console.log('Success!')
+//   ); 
+//   });
 
 
-    fs.writeFile('index.html', htmlString, err => {
-      console.log(err)
-    }) //remember 3 arguments: name of the file you're creating(html) 2: 
-    // console.log(response)  //the value does not need quotes
-  }
+
   
       
-      
-  );
+// init()
+
+
+
+
+
+
+
+
+// const generator = () => {
+    // return  
+//
+
+     //remember 3 arguments: name of the file you're creating 2: what is being written 3: callback function
+    // console.log(response)  //the value does not need quotes
+//
